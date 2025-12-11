62b3e1e3030106628eda24bbb05032b9d8cc322f
amorqww <faceobito@yandex.ru>
2025-12-11 16:47:30 +0500

    connect2

--- db_connect.php
+++ db_connect.php
@@ -2,6 +2,7 @@
 $dsn = "pgsql:host=localhost;dbname=quiz;user=admin;password= 123";
 try{
     $pdo = new PDO($dsn);
+    print_r("ПОДКЛЮЧИЛИСЬ");
 }
 catch(\PDOException $e){
     log("ОШИБКА!!!!!!!: " . $e->getMessage());
